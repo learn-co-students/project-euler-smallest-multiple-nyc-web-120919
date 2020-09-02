2855b03d5d58160c711e84970f88bf01ecfce370
flatironschool <danliu277@gmail.com>
2020-09-02 17:42:39 -0400

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,26 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+    attr_accessor :lcm
+
+    def initialize(input)
+        self.smallest_multiple(input)
+    end
+
+    def smallest_multiple(input)
+        @lcm = 1
+        (2..input).each do |x|
+            @lcm *= x / gcd(x)
+        end
+    end
+    
+    def gcd(low)
+        while low > 0
+            @lcm %= low
+            if @lcm == 0
+                return low
+            end
+            low %= @lcm
+        end
+        return @lcm
+    end
+end
